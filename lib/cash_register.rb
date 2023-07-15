f4d4d577956d31fe61351dc2401b601df61b1acf
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-07-15 14:18:10 +0300

    completed assignment

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,22 @@
 class CashRegister
-    attr_accessor :total, :discount, :items
+    attr_accessor :total, :discount, :items 
 
     def initialize(discount = 0)
         @discount = discount
         @total = 0
         @items = []
+        # @reciept = []
+        @last_price = 0
     end
 
-    def add_item(title,price,quantity = 0)
-        if quantity > 0
-            self.total += (price * quantity)
-            quantity.times { @items << title }
-        else
-            self.total += price
-            @items << title
-        end 
+    def add_item(title,price,quantity = 1)
+      
+        self.total += (price * quantity)
+        quantity.times { self.items << title }
+        @last_price = price * quantity
+        
+
+        
     end
 
     def apply_discount
@@ -30,7 +32,25 @@ class CashRegister
         @items
     end
 
+    def last_price
+        @last_price
+    end
+
     def void_last_transaction
-        self.total -= self.items.pop
+        self.total -= self.last_price
     end
 end
+
+
+# grocery = CashRegister.new
+# grocery.add_item("cake", 20, 3)
+# grocery.add_item("pen", 20)
+# grocery.add_item("eggs", 17, 1)
+
+# puts grocery.items
+# puts grocery.reciept.pop
+
+# puts grocery.total += grocery.reciept.pop
+
+# puts add_item("cake", 20, 3)
+# add_item("toothpaste", 100, 1)
